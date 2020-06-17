11eef423de3ad96fc54ab9900355697f7a57bda0
Ahmad Abdallah <47898887+AhmadAbdallah13@users.noreply.github.com>
2020-06-17 12:20:40 +0300

    Update
    Remove DetectSyntax because it doesn't support the Arabic language.

--- AWS-Comprehend-Functions.py
+++ AWS-Comprehend-Functions.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-text = "It is raining today in Seattle"
+text = ""
 
 def DetectDominantLanguage(text):
     print('Calling DetectDominantLanguage')
@@ -12,34 +12,25 @@ def DetectDominantLanguage(text):
 def DetectEntities(text):
     #You must specify the language of the input text.
     print('Calling DetectEntities')
-    print(json.dumps(comprehend.detect_entities(Text=text, LanguageCode='ar'), sort_keys=True, indent=4))
+    print(json.loads(json.dumps(comprehend.detect_entities(Text=text, LanguageCode='ar'), sort_keys=True, indent=4)))
     print('End of DetectEntities\n')
 
 def DetectKeyPhrases(text):
     #determine the key noun phrases used in text, You must specify the language of the input text.
     print('Calling DetectKeyPhrases')
-    print(json.dumps(comprehend.detect_key_phrases(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+    print(json.loads(json.dumps(comprehend.detect_key_phrases(Text=text, LanguageCode='ar'), sort_keys=True, indent=4)))
     print('End of DetectKeyPhrases\n')
 
 def DetectSentiment(text):
     #Detect sentiment, You must specify the language of the input text.
     print('Calling DetectSentiment')
-    print(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+    print(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='ar'), sort_keys=True, indent=4))
     print('End of DetectSentiment\n')
 
-def DetectSyntax(text):
-    #parse text to extract the individual words and determine the parts of speech for each word
-    #language is required
-    print('Calling DetectSyntax')
-    print(json.dumps(comprehend.detect_syntax(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
-    print('End of DetectSyntax\n')
-
-def DoEverything(text, DetectSyntaxx=True, DetectSentimentt=True,
+def DoEverything(text, DetectSentimentt=True,
                      DetectKeyPhrasess=True, DetectEntitiess=True, 
                      DetectDominantLanguages=True):
     
-    if DetectSyntaxx:
-        DetectSyntax(text)
     if DetectSentimentt:
         DetectSentiment(text)
     if DetectKeyPhrasess:
